3a01b8b8d0d6efd363559e448d6ec6c65cb92b35
YJK23333 <1755336771@qq.com>
2024-09-24 16:37:48 +0800

    Delete code

--- main.cpp
+++ main.cpp
@@ -9,15 +9,3 @@ int main()
 	return 0;
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-//Written the player of Windows
-=======
-//Written by Windows but there is something written by Linux
->>>>>>> 30c8ba3a43f352bfd5a7335d4629151294cfb611
-
-=======
-//Written by Linux(Ubuntu)
->>>>>>> 4a925beccdd687142c1958bd4901ef0451e75c6e
-
-
